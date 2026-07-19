09196b4ff669be6d1b2382438aa94bd4ffed5359
lkodos <tshagimardanov@gmail.com>
2026-07-19 14:36:21 +0300

    initial commit

---
+++ src/main/java/ru/lkodos/promsafe/PromSafeApplication.java
@@ -0,0 +1,13 @@
+package ru.lkodos.promsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromSafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromSafeApplication.class, args);
+    }
+
+}
